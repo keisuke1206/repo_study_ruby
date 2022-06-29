2205320984298ad2073162c8bf5f35cc83ffadb9
unknown <keisukeeee12061206@gmail.com>
2022-06-29 21:47:01 +0900

    hello.rbの編集

--- hello.rb
+++ hello.rb
@@ -1,1 +1,15 @@
 puts "hello"
+
+names = [1,2,3,4,5,6,7,8,9,10]
+names.each do |name|
+  puts name
+end
+
+
+(1..10).each do |number|
+if  number >= 10 then
+  puts "OK"
+ else number <=10
+   puts "NG"
+end
+end
