40111a9807ec7e0f9408be566f14515647f09c05
unknown <keisukeeee12061206@gmail.com>
2022-06-30 20:30:00 +0900

    numの追加

--- hello.rb
+++ hello.rb
@@ -7,9 +7,19 @@ names = [1,2,3,4,5,6,7,8,9,10]
 
 
 (1..10).each do |number|
-  if  number >= 10 then
+  if  number >=10
     puts "OK"
   else number <=10
     puts "NG"
+  end
+end
+
+nums = [17,87,92,335,32]
+  nums.each do |num|
+  case num
+    when num >= 10
+      puts "値の#{num}は50以上です"
+    else  num <=  10
+      puts "値の#{num}は50未満です"
   end
 end
