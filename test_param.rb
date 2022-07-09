ef6d72e2cff76d566ba05fca091f742835d5ea7f
unknown <keisukeeee12061206@gmail.com>
2022-07-09 23:21:11 +0900

    test_param.rbのインデント編集

--- test_param.rb
+++ test_param.rb
@@ -1,14 +1,16 @@
 puts ARGV[0]
 
-##ARGVで取れるのは文字列
+##ARGVで取れるのは文字列。
 val = ARGV[0]
-  puts "#{val}回の繰り返しを開始"
-  ##to_i は 文字列を整数に返すメソッド。
-    val.to_i.times do |i|
-      puts "#{i+1}回目の表示" 
-    end
-  puts "#{val}回の繰り返しを終了"
+puts "#{val}回の繰り返しを開始"
+
+##to_i は 文字列を整数に返すメソッド。
+val.to_i.times do |i|
+  puts "#{i+1}回目の表示" 
+end
+
+puts "#{val}回の繰り返しを終了"
 
 val = ARGV[0]
-puts val
-puts val.class 
+  puts val
+  puts val.class 
