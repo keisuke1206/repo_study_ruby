b3c48b04b33c7714b0cf7af5e47f3ca1b3c2d574
unknown <keisukeeee12061206@gmail.com>
2022-07-26 21:02:50 +0900

    配列について

--- test_param.rb
+++ test_param.rb
@@ -2,14 +2,14 @@ puts ARGV[0]
 
 ##ARGVで取れるのは文字列。
 val = ARGV[0]
-puts "#{val}回の繰り返しを開始"
+  puts "#{val}回の繰り返しを開始"
 
 ##to_i は 文字列を整数に返すメソッド。
 val.to_i.times do |i|
   puts "#{i+1}回目の表示" 
 end
 
-puts "#{val}回の繰り返しを終了"
+  puts "#{val}回の繰り返しを終了"
 
 val = ARGV[0]
   puts val
@@ -17,7 +17,7 @@ val = ARGV[0]
 
 val = ARGV[0]
 val.to_i.times do
- puts rand(1..100)
+  puts rand(1..100)
 end
 
 
@@ -47,7 +47,7 @@ end
 numbers1 = array1.sort
 numbers2 = array2.sort
 
-numbers1.zip(numbers2) do |x,y|
- sum = x + y
-  puts "1つ目の値「#{x}」と「#{y}」を足すと「#{sum}」です"
+numbers1.zip(numbers2).each_with_index do |(x,y), idx|
+sum = x + y
+  puts "#{idx + 1}つ目の値「#{x}」と「#{y}」を足すと「#{sum}」です"
 end
