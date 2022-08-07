6527184f754de53c49236b63a9c06c417d9dfd94
unknown <keisukeeee12061206@gmail.com>
2022-08-07 18:27:47 +0900

    関数のエラー

---
+++ sample_error.rb
@@ -0,0 +1,13 @@
+val1 = ARGV[1]
+val2 = ARGV[2]
+
+def sample_array1(val1,val2)
+  puts sum1 = val1 + val2
+end
+
+def sample_array2(val1,val2)
+  puts sum2 = val1 * val2
+end
+
+puts sample_array1(val1,val2)
+puts sample_array2(val1,val2)
