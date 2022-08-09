74ff921a3cd03c70cf198152250140bdc179086e
unknown <keisukeeee12061206@gmail.com>
2022-08-09 22:24:51 +0900

    一時保存

--- sample_error.rb
+++ sample_error.rb
@@ -1,13 +1,30 @@
-val1 = ARGV[1]
-val2 = ARGV[2]
+val1 = ARGV[0].to_i
+val2 = ARGV[1].to_i
+val3 = ARGV[3].to_i
 
-def sample_array1(val1,val2)
-  puts sum1 = val1 + val2
+def test_array1(val1,val2,val3)
+  return test_array1(val1,val2,val3).min
 end
 
-def sample_array2(val1,val2)
-  puts sum2 = val1 * val2
+def test_array2(val1,val2,val3)
+  return test_array2(val1,val2,val3).max
 end
 
-puts sample_array1(val1,val2)
-puts sample_array2(val1,val2)
+valmin = test_array1(val1,val2,val3)
+valmax = test_array2(val1,val2,val3)
+valsum = valmin + valmax
+
+def test_array3(val1,val2,val3)
+  beside = test_array3(val1,val2,val3).where.not(valmax,valmin)
+  besides = beside * 2
+
+if sum > besides
+  puts "大きい"
+else
+  puts 小さい
+end
+end
+
+"関数1で一番小さな値を調べた結果は#{test_array1(val1,val2,val3)}です。"
+"関数2で一番小さな値を調べた結果は#{test_array2(val1,val2,val3)}です。"
+"一番大きな値と一番小さな値を足した値は、真ん中の値の2倍よりも#{test_array3(val1,val2,val3)}です。"
--- test_param.rb
+++ test_param.rb
@@ -169,3 +169,35 @@ sum3 =sum2 - sum1
 
 puts "関数2の結果は#{sample_main2(val1,val2)}で関数1の結果は#{sample_main1(val1,val2)}なので、
 減算すると#{sum3}です"
+
+
+val1 = ARGV[0].to_i
+val2 = ARGV[1].to_i
+val3 = ARGV[3].to_i
+
+def test_array1(val1,val2,val3)
+  return test_array1(val1,val2,val3).min
+end
+
+def test_array2(val1,val2,val3)
+  return test_array2(val1,val2,val3).max
+end
+
+valmin = test_array1(val1,val2,val3)
+valmax = test_array2(val1,val2,val3)
+valsum = valmin + valmax
+
+def test_array3(val1,val2,val3)
+  beside = test_array3(val1,val2,val3).where.not(valmax,valmin)
+  besides = beside * 2
+
+if sum > besides
+  puts "大きい"
+else
+  puts 小さい
+end
+end
+
+"関数1で一番小さな値を調べた結果は#{test_array1(val1,val2,val3)}です。"
+"関数2で一番小さな値を調べた結果は#{test_array2(val1,val2,val3)}です。"
+"一番大きな値と一番小さな値を足した値は、真ん中の値の2倍よりも#{test_array3(val1,val2,val3)}です。"
