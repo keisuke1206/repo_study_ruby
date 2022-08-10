d7e6dbb689abf3699faf6c1d8343254dc4ddd31d
unknown <keisukeeee12061206@gmail.com>
2022-08-10 22:56:11 +0900

    関数について

--- test_param.rb
+++ test_param.rb
@@ -173,32 +173,37 @@ puts "関数2の結果は#{sample_main2(val1,val2)}で関数1の結果は#{sampl
 
 val1 = ARGV[0].to_i
 val2 = ARGV[1].to_i
-val3 = ARGV[3].to_i
+val3 = ARGV[2].to_i
 
 def test_array1(val1,val2,val3)
-  return test_array1 = [val1,val2,val3].min  
+  test_array1 = [val1,val2,val3]  
+  return test_array1.min
 end
 
 def test_array2(val1,val2,val3)
-  return test_array2 = [val1,val2,val3].max
+  test_array2 = [val1,val2,val3]
+  return test_array2.max
 end
 
 valmin = test_array1(val1,val2,val3)
 valmax = test_array2(val1,val2,val3)
 valsum = valmin + valmax
 
+
 def test_array3(val1,val2,val3)
-num = [val1,val2,val3]
-beside = num.where.not(valmax,valmin)
-besides = beside * 2
+test_array3 = [val1,val2,val3]
+num1 = test_array3.sort
+beside = num1[1] * 2
 
-if sum > besides
-  puts "大きい"
+if valsum > beside
+  puts "一番大きな値と一番小さな値を足した値は、
+  真ん中の値の2倍よりも大きいです。"
 else
-  puts 小さい
+  puts "一番大きな値と一番小さな値を足した値は、
+  真ん中の値の2倍よりも小さいです。"
 end
 end
 
-"関数1で一番小さな値を調べた結果は#{test_array1(val1,val2,val3)}です。"
-"関数2で一番小さな値を調べた結果は#{test_array2(val1,val2,val3)}です。"
-"一番大きな値と一番小さな値を足した値は、真ん中の値の2倍よりも#{test_array3(val1,val2,val3)}です。"
+puts "関数1で一番小さな値を調べた結果は#{test_array1(val1,val2,val3)}です。"
+puts "関数2で一番大きな値を調べた結果は#{test_array2(val1,val2,val3)}です。"
+puts test_array3(val1,val2,val3)
