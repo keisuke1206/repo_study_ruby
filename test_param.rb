57f1b28629e0cd5510733736a2b5e22a2bd10a2a
unknown <keisukeeee12061206@gmail.com>
2022-08-11 21:38:18 +0900

    関数について

--- test_param.rb
+++ test_param.rb
@@ -185,25 +185,23 @@ def test_array2(val1,val2,val3)
   return test_array2.max
 end
 
-valmin = test_array1(val1,val2,val3)
-valmax = test_array2(val1,val2,val3)
-valsum = valmin + valmax
-
-
 def test_array3(val1,val2,val3)
+
+min = test_array1(val1,val2,val3)
+max = test_array2(val1,val2,val3)
+valsum = min + max
+
 test_array3 = [val1,val2,val3]
 num1 = test_array3.sort
 beside = num1[1] * 2
 
 if valsum > beside
-  puts "一番大きな値と一番小さな値を足した値は、
-  真ん中の値の2倍よりも大きいです。"
+  puts "大きい"
 else
-  puts "一番大きな値と一番小さな値を足した値は、
-  真ん中の値の2倍よりも小さいです。"
+  puts "小さい"
 end
 end
 
 puts "関数1で一番小さな値を調べた結果は#{test_array1(val1,val2,val3)}です。"
 puts "関数2で一番大きな値を調べた結果は#{test_array2(val1,val2,val3)}です。"
-puts test_array3(val1,val2,val3)
+puts ("一番大きな値と一番小さな値を足した値は、真ん中の値の2倍よりも#{test_array3(val1,val2,val3)}です。")
