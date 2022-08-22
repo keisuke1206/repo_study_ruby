4e372118d22abc638106dbd035840df2f21ba972
unknown <keisukeeee12061206@gmail.com>
2022-08-23 00:38:25 +0900

    関数エラー

--- sample_error.rb
+++ sample_error.rb
@@ -1,4 +1,18 @@
-sample_numbers = [val1,val2,val3,val4,val5]
-sum = number * 3 + 10
-sum1 = sum.sum
-puts sum1 
+val1 = ARGV[0].to_i
+val2 = ARGV[1].to_i
+val3 = ARGV[2].to_i
+
+def conditions(results)
+  results = [val1,val2,val3]
+  results.each do |result|
+  if result < 10
+    puts "10より小さい"
+  elsif result > 10
+    puts "10より大きい"
+  else
+    puts "10と同じ"
+  end
+  end
+end
+
+puts conditions(results)
