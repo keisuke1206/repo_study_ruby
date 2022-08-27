17dfb723006fc7f60d8640e3ae203efc2acc4d98
unknown <keisukeeee12061206@gmail.com>
2022-08-28 06:43:06 +0900

    値渡しについて

--- test_param.rb
+++ test_param.rb
@@ -239,16 +239,17 @@ val2 = ARGV[1].to_i
 val3 = ARGV[2].to_i
 
 def conditions(results)
-  results = [val1,val2,val3]
-  results.each do |result|
-  if result < 10
-    puts "10より小さい"
-  elsif result > 10
-    puts "10より大きい"
+  if results < 10
+    return "10より小さい"
+  elsif results > 10
+    return "10より大きい"
   else
-    puts "10と同じ"
-  end
+    return "10と同じ"
   end
 end
 
-puts conditions(results)
+results= [val1,val2,val3]
+
+conditions(results)
+
+puts results
