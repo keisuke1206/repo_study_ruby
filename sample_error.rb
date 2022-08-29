66082f6b3bdee6d99ef14c764ae4a1fcda9cfd08
unknown <keisukeeee12061206@gmail.com>
2022-08-29 23:56:23 +0900

    引数について

--- sample_error.rb
+++ sample_error.rb
@@ -1,19 +1,21 @@
+def conditions(results)
+  results = []
+  results.each do |result|
+  if result < 10
+    puts "10より小さい"
+  elsif result > 10
+    puts "10より大きい"
+  else
+    puts "10と同じ"
+  end
+end
+  return results
+end
+
 val1 = ARGV[0].to_i
 val2 = ARGV[1].to_i
 val3 = ARGV[2].to_i
 
-def conditions(results)
-  if results < 10
-    return "10より小さい"
-  elsif results > 10
-    return "10より大きい"
-  else
-    return "10と同じ"
-  end
-end
+results = [val1,val2,val3]
 
-results= [val1,val2,val3]
-
-conditions(results)
-
-puts results
+puts conditions(results)
