f893b463ff0ff2a704c33a5d0cb9b8d2b602d1e2
unknown <keisukeeee12061206@gmail.com>
2022-08-30 22:34:31 +0900

    関数について

--- sample_error.rb
+++ sample_error.rb
@@ -1,5 +1,9 @@
+val1 = ARGV[0].to_i
+val2 = ARGV[1].to_i
+val3 = ARGV[2].to_i
+
+
 def conditions(results)
-  results = []
   results.each do |result|
   if result < 10
     puts "10より小さい"
@@ -9,13 +13,8 @@ def conditions(results)
     puts "10と同じ"
   end
 end
-  return results
 end
 
-val1 = ARGV[0].to_i
-val2 = ARGV[1].to_i
-val3 = ARGV[2].to_i
+params = [val1,val2,val3]
 
-results = [val1,val2,val3]
-
-puts conditions(results)
+conditions(params)
