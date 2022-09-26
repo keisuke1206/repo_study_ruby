c42bf29c0b4b5bcddb7c188415e8918e31a0a69b
unknown <keisukeeee12061206@gmail.com>
2022-09-26 20:23:27 +0900

    requireメソッド

--- test_object.rb
+++ test_object.rb
@@ -1,13 +1,4 @@
-class Text
-  def initialize(text)
-    @text = text
-  end
-
-  def text_column
-    puts "受け取った文字列は「#{@text}」です。この文字列の長さは「#{@text.length}」です。"
-  end
-end
-
+require './test_object_class.rb'
 text1 = Text.new("apple")
 text2 = Text.new("orange")
 text3 = Text.new("strawberry")
