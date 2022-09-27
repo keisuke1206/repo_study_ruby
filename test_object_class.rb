aeb813ce445250290073bd4316ac8a4a246a6aea
unknown <keisukeeee12061206@gmail.com>
2022-09-27 20:37:28 +0900

    オブジェクトについて

--- test_object_class.rb
+++ test_object_class.rb
@@ -7,3 +7,26 @@ class Text
     puts "受け取った文字列は「#{@text}」です。この文字列の長さは「#{@text.length}」です。"
   end
 end
+
+
+class Sum
+  def initialize(val)
+    @val = val.to_i
+  end
+
+  def addition_sum
+    addition_sum = 0
+    for i in 1..@val do
+      addition_sum += i
+    end
+      puts "パラメータは「#{@val}」です。1からすべて加算すると「#{addition_sum}」です。"
+  end
+
+  def multiplication_sum
+    multiplication_sum = 1
+    for i in 1..@val do
+      multiplication_sum *= i
+    end
+      puts "パラメータは「#{@val}」です。1からすべて加算すると「#{multiplication_sum}」です。"
+  end
+end
