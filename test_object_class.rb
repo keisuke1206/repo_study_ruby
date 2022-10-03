1946070147aaf45d84afc5859c81159f4e99b40d
unknown <keisukeeee12061206@gmail.com>
2022-10-03 20:36:43 +0900

    classについて

--- test_object_class.rb
+++ test_object_class.rb
@@ -30,3 +30,30 @@ class Sum
       puts "パラメータは「#{@val}」です。1からすべて加算すると「#{multiplication_sum}」です。"
   end
 end
+
+
+class Number
+  def initialize(plus,minus,increment,random,val)
+    @plus = plus
+    @minus = minus
+    @increment = increment
+    @random = random
+    @val = val
+  end
+
+  def process
+    if @plus
+      puts "処理パターン:#{plus}"
+    else
+      puts "処理パターン:#{minus}"
+    end
+  end
+
+  def arrangement
+    if @increment
+      puts " 配列パターン:#{increment}"
+    else
+      puts "配列パターン:#{random}"
+    end
+  end
+end
