6604426ffb39921bf552e4f5223034daf4053142
unknown <keisukeeee12061206@gmail.com>
2022-10-03 21:41:20 +0900

    パラメータを呼び出すためには

--- sample_error.rb
+++ sample_error.rb
@@ -1,21 +1,25 @@
-val1 = ARGV[0].to_i
-val2 = ARGV[1].to_i
-val3 = ARGV[2].to_i
+class Number
+  def initialize(plus,minus,increment,random,val)
+    @plus = plus
+    @minus = minus
+    @increment = increment
+    @random = random
+    @val = val
+  end
 
-def conditions(results)
-  results.each do |result|
-  if result < 10
-    return "10より小さい"
-  elsif result > 10
-    return "10より大きい"
-  else
-    return "10と同じ"
+  def process
+    if @plus
+      puts "処理パターン:#{plus}"
+    else
+      puts "処理パターン:#{minus}"
+    end
+  end
+
+  def arrangement
+    if @increment
+      puts "配列パターン:#{increment}"
+    else
+      puts "配列パターン:#{random}"
+    end
   end
 end
-end
-
-params = [val1,val2,val3]
-
-params.each do |param|
-puts "パラメータの値#{param}は#{conditions(params)}で、計算結果はです。"
-end 
